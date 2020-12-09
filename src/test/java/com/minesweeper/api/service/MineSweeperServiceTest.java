5356d3b91eeb96a21001e9662c775c5718d26f8f
jrivero <rivero.johnatan@gmail.com>
2020-12-09 19:39:10 -0300

    Add test for pause/resume behaviour

--- src/test/java/com/minesweeper/api/service/MineSweeperServiceTest.java
+++ src/test/java/com/minesweeper/api/service/MineSweeperServiceTest.java
@@ -2,6 +2,7 @@ package com.minesweeper.api.service;
 
 import com.minesweeper.api.dto.MineSweeperRequest;
 import com.minesweeper.api.model.MineSweeper;
+import com.minesweeper.api.model.Status;
 import com.minesweeper.api.model.exception.MinesweeperApiException;
 import com.minesweeper.api.repository.MineSweeperRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,10 +13,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Date;
+import java.util.Optional;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 
 @SpringBootTest
 public class MineSweeperServiceTest {
@@ -72,6 +76,45 @@ public class MineSweeperServiceTest {
             fail();
         } catch (MinesweeperApiException e) {
             assertEquals(e.getMessage(),"Invalid request. Columns, Rows and Bombs should be greater that 0");
+        }
+    }
+
+    @Test
+    void pauseActiveGame(){
+        String gameId = UUID.randomUUID().toString();
+        this.mineSweeper.setId(gameId);
+        Mockito.when(mineSweeperRepository.findById(eq(gameId))).thenReturn(Optional.of(mineSweeper));
+        Mockito.when(mineSweeperRepository.save(eq(mineSweeper))).thenReturn(mineSweeper);
+        MineSweeper gamePaused = mineSweeperService.pauseResumeMineSweeper(gameId);
+
+        assertEquals(gamePaused.getStatus(), Status.PAUSED);
+    }
+
+    @Test
+    void resumeAPausedGame(){
+        String gameId = UUID.randomUUID().toString();
+        this.mineSweeper.setId(gameId);
+        this.mineSweeper.setStatus(Status.PAUSED);
+        Mockito.when(mineSweeperRepository.findById(eq(gameId))).thenReturn(Optional.of(mineSweeper));
+        Mockito.when(mineSweeperRepository.save(eq(mineSweeper))).thenReturn(mineSweeper);
+        MineSweeper gamePaused = mineSweeperService.pauseResumeMineSweeper(gameId);
+
+        assertEquals(gamePaused.getStatus(), Status.ACTIVE);
+    }
+
+    @Test
+    void pauseAnOverGameThrowsMinesweeperApiException(){
+        String gameId = UUID.randomUUID().toString();
+        this.mineSweeper.setId(gameId);
+        this.mineSweeper.setStatus(Status.GAME_OVER);
+        Mockito.when(mineSweeperRepository.findById(eq(gameId))).thenReturn(Optional.of(mineSweeper));
+        Mockito.when(mineSweeperRepository.save(eq(mineSweeper))).thenReturn(mineSweeper);
+
+        try{
+            mineSweeperService.pauseResumeMineSweeper(gameId);
+            fail();
+        } catch(MinesweeperApiException e){
+            assertEquals(e.getMessage(),"The game is over and could not be resumed/paused");
         }
     }
 
